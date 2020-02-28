15c58a3e31c4e1a1c00ff16e29e69ce028376026
Casper Dik <casperdik3@gmail.com>
2020-02-28 13:19:07 +0100

    Tutorial 4

---
+++ week 4/Tutorial 4.py
@@ -0,0 +1,53 @@
+#exercises
+#1
+day_number = int(input("daynumber?: "))
+
+if day_number == 0:
+    print("Sun")
+if day_number == 1:
+    print("Mon")
+if day_number == 2:
+    print("Tue")
+if day_number == 3:
+    print("Wed")
+if day_number == 4:
+    print("Thu")
+if day_number == 5:
+    print("Fri")
+if day_number == 6:
+    print("Sat")
+
+#2
+day_number2 = int(input("daynumber?: "))
+length_stay = int(input("Length of your stay?: "))
+
+return_day = (length_stay % 7 ) + day_number
+
+if day_number2 == 0:
+    print("Sun")
+if day_number2 == 1:
+    print("Mon")
+if day_number2 == 2:
+    print("Tue")
+if day_number2 == 3:
+    print("Wed")
+if day_number2 == 4:
+    print("Thu")
+if day_number2 == 5:
+    print("Fri")
+if day_number2 == 6:
+    print("Sat")
+
+#3
+#a a < b
+#b a <= b
+#c a <= b and day != 3
+#d a <= b and day == 3
+
+#4
+#a 3 equals 3
+#b 3 unequal to 3
+#c 3 greater or equal to 4
+#d 3 true if 3 is not smaller than 4
+
+#5
