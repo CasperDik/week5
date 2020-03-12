aa0e811a7946e1e8ae70e61037bed06ab9c8e1a1
Casper Dik <casperdik3@gmail.com>
2020-03-12 16:48:55 +0100

    4.9

--- week 5/exercise 4.9 - 7.py
+++
@@ -1,7 +0,0 @@
-def sum_integers(n):
-    sum = 0
-    for i in range(n+1):
-        sum = sum + i
-    print(sum)
-
-sum_integers(10)
--- week 5/exercise 4.9 - 8.py
+++
@@ -1,6 +0,0 @@
-def area_of_circle(r):
-    import math
-    area = math.pi * (r**2)
-    print(area)
-
-area_of_circle(12)
