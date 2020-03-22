56c1034fd32926ff2604663e1a0ad6ae6d47fa14
Casper Dik <casperdik3@gmail.com>
2020-03-22 14:56:56 +0100

    changes

--- assessment_basic_track/collect_terms.py
+++ assessment_basic_track/collect_terms.py
@@ -3,15 +3,14 @@ def createlist(n):
     for i in range(n):                  #inputs
         x = input("Please write here the bingo terms: ")
         terms.append(x)
-    print(terms)
     myfile= open("terms.txt", "a+")
     for i in terms:                     #terms to file
         myfile.write(i)
         myfile.write("\n")
+    # Check if a term hasn't been added before
+    # Register how often a term has been added to indicate how relevant a term is
 
 n = int(input("how many bingo terms do you want to add to the list: "))
 createlist(n)
 
-# Check if a term hasn't been added before
-# Register how often a term has been added to indicate how relevant a term is
 
--- assessment_basic_track/draw_terms.py
+++ assessment_basic_track/draw_terms.py
@@ -5,11 +5,14 @@ def draw_terms():
     read = open("terms.txt").read()
     read = read[0:]
     list = read.split()
-    n = len(list)
+    list_2 = list.copy()
+    n = len(list_2)
+
 
     for i in range(n):
-        random_word = random.choice(list)
+        random_word = random.choice(list_2)
         print(random_word)
-        time.sleep(60)              #wait 60 seconds before looping again
+        list_2.remove(random_word)
+        time.sleep(0.2)              #wait 60 seconds before looping again
 
 draw_terms()
