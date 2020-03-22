027d03fe623cff2c69a840cd5b1bc19f0b1b3446
Casper Dik <casperdik3@gmail.com>
2020-03-22 15:04:41 +0100

    changes

--- assessment_basic_track/create_card.py
+++ assessment_basic_track/create_card.py
@@ -5,12 +5,13 @@ def create_bingo_card():
     read = open("terms.txt").read()
     read = read[0:]
     list = read.split()
+    list_3 = list.copy()
     card = []
 
     for i in range(25):
-        random_word = random.choice(list)
+        random_word = random.choice(list_3)
         card.append(random_word)           # create list with 25 terms
-    #make sure that they are UNIQUE terms
+        list_3.remove(random_word)
 
     bingo_card = numpy.array(card)
     bingo_card = bingo_card.reshape([5,5])      # reshape list in 5x5 grid
