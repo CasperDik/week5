a310e05ac84bea6c373de6796efa22564c5d2907
Casper Dik <casperdik3@gmail.com>
2020-03-24 11:26:34 +0100

    bingo

--- assessment_basic_track/check_terms.py
+++ assessment_basic_track/check_terms.py
@@ -3,6 +3,8 @@ def check_terms():
         import time
         import numpy
 
+        t = 2                             #pause time
+        x=0                                 #dummy to break loop
         read = open("terms.txt").read()
         read = read[0:]
         list = read.split()
@@ -14,20 +16,39 @@ def check_terms():
 
         for i in range(n):
             print("This is round number", (i+1))
-            time.sleep(2)
+            time.sleep(t)
             random_word = random.choice(list_2)
             print("The drawn word is: ", random_word)
-            time.sleep(2)
+            time.sleep(t)
             list_2.remove(random_word)
             if random_word in bingocard2:
                 print("YES!", random_word, "is on your bingocard!!")
-                time.sleep(2)
+                time.sleep(t)
                 bingocard2[bingocard2.index(random_word)] = "X"
                 bc = numpy.array(bingocard2)
                 bc = bc.reshape([5, 5])
                 print("This is your current bingocard: \n", bc)
+                for i in range(5):                      #test for bingo on rows
+                    j=0
+                    if bc[i, j] == bc[i, j + 1] == bc[i, j + 2] == bc[i, j + 3] == bc[i, j + 4] == "X":
+                        x=1
+                for j in range(5):
+                    i=0                                #test for bingo colums
+                    if bc[i, j] == bc[i+1, j] == bc[i+2, j] == bc[i+3, j] == bc[i+4, j] == "X":
+                        x=1
+                j=0
+                i=0                             #test for diagonal bingo
+                if bc[i, j] == bc[i+1, j+1] == bc[i+2, j+2] == bc[i+3, j+3] == bc[i+4, j+4] == "X":
+                        x=1
+                                                # test for diagonal bingo
+                j=4
+                if bc[i, j] == bc[i+1, j-1] == bc[i+2, j-2] == bc[i+3, j-3] == bc[i+4, j-4] == "X":
+                    x=1
+            if x == 1:
+                print("You have bingo!!! Shout BINGO and claim your price!")
+                break
             else:
-                print("Unfortunate,", random_word , "is not on your bingocard")
-            time.sleep(3)  # wait x seconds before looping again
+                print("Unfortunate,", random_word , "is not on your bingocard.")
+            time.sleep(t)  # wait x seconds before looping again
 
 check_terms()
--- assessment_basic_track/play_bingo.py
+++ assessment_basic_track/play_bingo.py
@@ -1,3 +1,6 @@
+#bingo program
+#made by Casper Dik
+
 def createlist(n):
     terms = list()
     for i in range(n):                  #inputs
@@ -36,54 +39,58 @@ def generate_bingo_cards():
         create_bingo_card()
         new_card = input("Do you want to generate a new bingo card: ")
 
-def draw_terms():
+def check_terms():
     import random
     import time
+    import numpy
 
+    t = 2  # pause time
+    x = 0  # dummy to break loop
     read = open("terms.txt").read()
     read = read[0:]
     list = read.split()
     list_2 = list.copy()
     n = len(list_2)
 
+    bingocard = open("bingo_card.txt").read()
+    bingocard2 = bingocard.split()
 
     for i in range(n):
+        print("This is round number", (i + 1))
+        time.sleep(t)
         random_word = random.choice(list_2)
-        print(random_word)
+        print("The drawn word is: ", random_word)
+        time.sleep(t)
         list_2.remove(random_word)
-        time.sleep(10)              #wait 10 seconds before looping again
-
-def check_terms():
-        import random
-        import time
-        import numpy
-
-        read = open("terms.txt").read()
-        read = read[0:]
-        list = read.split()
-        list_2 = list.copy()
-        n = len(list_2)
-
-        bingocard = open("bingo_card.txt").read()
-        bingocard2 = bingocard.split()
-
-        for i in range(n):
-            print("This is round number", (i + 1))
-            time.sleep(2)
-            random_word = random.choice(list_2)
-            print("The drawn word is: ", random_word)
-            time.sleep(2)
-            list_2.remove(random_word)
-            if random_word in bingocard2:
-                print("YES!", random_word, "is on your bingocard!!")
-                time.sleep(2)
-                bingocard2[bingocard2.index(random_word)] = "X"
-                bc = numpy.array(bingocard2)
-                bc = bc.reshape([5, 5])
-                print("This is your current bingocard: \n", bc)
-            else:
-                print("Unfortunate,", random_word, "is not on your bingocard")
-            time.sleep(3)  # wait x seconds before looping again
+        if random_word in bingocard2:
+            print("YES!", random_word, "is on your bingocard!!")
+            time.sleep(t)
+            bingocard2[bingocard2.index(random_word)] = "X"
+            bc = numpy.array(bingocard2)
+            bc = bc.reshape([5, 5])
+            print("This is your current bingocard: \n", bc)
+            for i in range(5):              # test for bingo on rows
+                j = 0
+                if bc[i, j] == bc[i, j + 1] == bc[i, j + 2] == bc[i, j + 3] == bc[i, j + 4] == "X":
+                    x = 1
+            for j in range(5):
+                i = 0                       # test for bingo on columns
+                if bc[i, j] == bc[i + 1, j] == bc[i + 2, j] == bc[i + 3, j] == bc[i + 4, j] == "X":
+                    x = 1
+            j = 0
+            i = 0                           # test for diagonal bingo
+            if bc[i, j] == bc[i + 1, j + 1] == bc[i + 2, j + 2] == bc[i + 3, j + 3] == bc[i + 4, j + 4] == "X":
+                x = 1
+                                        # test for diagonal bingo
+            j = 4
+            if bc[i, j] == bc[i + 1, j - 1] == bc[i + 2, j - 2] == bc[i + 3, j - 3] == bc[i + 4, j - 4] == "X":
+                x = 1
+        if x == 1:
+            print("You have bingo!!! Shout BINGO and claim your price!")
+            break
+        else:
+            print("Unfortunate,", random_word, "is not on your bingocard.")
+        time.sleep(t)  # wait x seconds before looping again
 
 
 def play_bingo():
@@ -95,11 +102,8 @@ def play_bingo():
 
 play_bingo()
 
-
-
 #to do
+#improve storing terms to list --> not ask for number but break loop in another way
 #Check if a term hasn't been added before
 #(extra) Register how often a term has been added to indicate how relevant a term is
-#(extra) When a row, column or diagonal has been completed indicate to the user they should call out "Bingo!"
-
-#print("You have bingo!!! Shout "BINGO" and claim your price!")
+#layout array?
